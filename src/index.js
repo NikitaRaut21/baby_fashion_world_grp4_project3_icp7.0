8568e44cbc92fc3330e055437da6dd730f77ec21
priyankaphadke12 <priyankaphadke14@gmail.com>
2024-06-15 21:03:57 +0530

    Added Folder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import About from "./views/About/About";
 
 import Review from "./views/Review/Review";
 
-
+import Contact from './views/Contact/Contact';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<h1>baby_fashion_world_grp4_project3_icp7.0</h1>);
@@ -21,3 +21,13 @@ root.render(<h1>baby_fashion_world_grp4_project3_icp7.0</h1>);
             else{
                 root.render(<h1>404 Page Not Found</h1>)
             }
+
+            if (path === "/Contact")
+                {
+                    root.render(<Contact />)
+                }
+                
+                    else{
+                        root.render(<Contact/>);
+
+                    }      
