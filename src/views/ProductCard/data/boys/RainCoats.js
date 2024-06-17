7d96bd0e49f082adb06190eca18bda42caff109d
sakshishewale2808 <sakshishewale2808@gmail.com>
2024-06-17 19:13:31 +0530

    raincoat data of boy added

---
+++ src/views/ProductCard/data/boys/RainCoats.js
@@ -0,0 +1,45 @@
+const boysRaincoatData = [
+    {
+      id: 11,
+      content: 'Waterproof Raincoat for Boys',
+      productsimgs: 'https://cdn.fcglcdn.com/brainbees/images/products/300x364/13347042a.webp',
+      date:'₹450',
+      Price: '₹350',
+      Offer:'(40% off)',
+      Technology: ['Waterproof', 'Breathable'],
+      category: 'raincoat'
+    },
+    {
+      id: 12,
+      content: 'Reflective Raincoat for Boys',
+      productsimgs: 'https://cdn.fcglcdn.com/brainbees/images/products/300x364/13347051b.jpg',
+      date:'₹450',
+      Price: '₹350',
+      Offer:'(40% off)',
+      Technology: ['Reflective', 'Water Resistant'],
+      category: 'raincoat'
+    },
+    {
+      id: 13,
+      content: 'Lightweight Raincoat for Boys',
+      productsimgs: 'https://cdn.fcglcdn.com/brainbees/images/products/300x364/13347142a.webp',
+     date:'₹450',
+      Price: '₹350',
+      Offer:'(40% off)',
+      Technology: ['Lightweight', 'Windproof'],
+      category: 'raincoat'
+    },
+    {
+      id: 14,
+      content: 'Fashionable Raincoat for Boys',
+      productsimgs: 'https://cdn.fcglcdn.com/brainbees/images/products/300x364/13011038a.webp',
+      date:'₹450',
+      Price: '₹350',
+      Offer:'(40% off)',
+      Technology: ['Fashionable Design', 'Water Repellent'],
+      category: 'raincoat'
+    },
+  ];
+  
+  export default boysRaincoatData;
+  
