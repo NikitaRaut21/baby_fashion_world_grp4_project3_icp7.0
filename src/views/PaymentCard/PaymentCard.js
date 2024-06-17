b502a37a0c68dbad787bc4e0dcaaa9f51e596467
sakshilambe1997 <sakshilambe1997@gmail.com>
2024-06-17 21:41:52 +0530

    payment card added

---
+++ src/views/PaymentCard/PaymentCard.js
@@ -0,0 +1,14 @@
+import "./PaymentCard.css"
+
+function PaymentCard(){
+    return(
+        <>
+        <div >
+           
+           <h1>hii</h1> 
+        </div>
+        </>
+    )
+}
+
+export default PaymentCard
