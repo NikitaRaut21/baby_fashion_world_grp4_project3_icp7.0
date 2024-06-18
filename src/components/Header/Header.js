bb2be4a4c4f0095713845fbbf6e439c842ccc553
rutu1518 <rutujamore1508@gmail.com>
2024-06-18 16:54:56 +0530

    header section is linked

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,10 +12,10 @@ function Header() {
     <div className='Navbar-Container'>
       <img  src={logo} className="header-logo"/>
       <Link to ='/' className='nav-menu'>Home</Link>
-      <Link to ='/blogs' className='nav-menu'>Blogs</Link>
+      <Link to ='/BoyGirl' className='nav-menu'>Category</Link>
       <Link to ='/about' className='nav-menu'>About</Link>
       <Link to ='/contact' className='nav-menu'>Contact</Link>
-      <Link to ='/review' className='nav-menu'>Shopping List</Link>
+      <Link to ='/shopping' className='nav-menu'>Shopping List</Link>
       <img  src={user} className="userr-logo"/>
       <img  src={cart} className="cart-logo"/>
     </div>
